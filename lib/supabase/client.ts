bc2ced51970fcc98646a8d42fdadf8479e6a08b2
phalvarengapinto <pedroalvarenga.particular@gmail.com>
2025-09-30 19:16:54 -0300

    Create client.ts

---
+++ lib/supabase/client.ts
@@ -0,0 +1,7 @@
+'use client'
+import { createClient as createBrowserClient } from '@supabase/supabase-js'
+
+export const supabase = createBrowserClient(
+process.env.NEXT_PUBLIC_SUPABASE_URL!,
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
