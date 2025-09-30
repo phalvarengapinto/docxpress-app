475e7bbf0b0020eaf89692ab485647fe013c8118
phalvarengapinto <pedroalvarenga.particular@gmail.com>
2025-09-30 19:48:43 -0300

    Delete lib/supabase directory

--- lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-import { createClient as createBrowserClient } from '@supabase/supabase-js'
-
-export const supabase = createBrowserClient(
-process.env.NEXT_PUBLIC_SUPABASE_URL!,
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
